24cff314d17bc9b1a2a442619cd4f1909a193db0
Dominik Müller <dominik@mueteam.de>
2022-09-02 09:26:35 +0200

    adjusted repo links for new username

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  title: 'wheelpick.local',
-  repoLink: 'https://github.com/Wurmloch/wheelpick',
-  madeByName: 'Dominik Müller',
-  madeByLink: 'https://dominikmueller.dev',
+  title: "wheelpick.local",
+  repoLink: "https://github.com/domudev/wheelpick",
+  madeByName: "Dominik Müller",
+  madeByLink: "https://dominikmueller.dev",
 };
